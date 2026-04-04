20b1a1e219452f208369b939d806bac06d6899e7
Sai Sathwik <sathwiksai743@gmail.com>
2026-04-04 22:33:32 +0530

    Fix Render 404: root route, MongoDB, dynamic port

--- server.js
+++ server.js
@@ -187,10 +187,10 @@ app.use(express.static(path.join(__dirname, 'public'), {
     }
 }));
 
-app.get(['../friendProfile.html', '../FriendProfile.html', '../Friendprofile.html', '../friendprofile.html'], (req, res) => {
+app.get(/\/friendprofile\.html$/i, (req, res) => {
     res.sendFile(path.join(__dirname, 'friendprofile.html'));
 });
-app.get(['/search.html', '/Search.html'], (req, res) => {
+app.get(/\/search\.html$/i, (req, res) => {
     res.sendFile(path.join(__dirname, 'search.html'));
 });
 
